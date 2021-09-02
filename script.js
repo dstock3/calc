55c710b72c5e5086626ade1a8a8bdb5cf52c0f54
David Stockdale <dstock3@gmail.com>
2021-09-02 16:18:50 -0500

    Refactored code so that an instance of display is instantiated when the operate function is called.

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const calcButtons = () => {
     return { elementArray, operators }
 };
 
-const display = (() => {
+const display = () => {
     let array = [];
 
     const clear = () => {
@@ -88,7 +88,6 @@ const display = (() => {
     }
 
     const clearArray = () => {
-        clear();
         array = [];
         return array;
     }
@@ -101,7 +100,7 @@ const display = (() => {
     }
 
     return { array, clearArray, addToArray, pushToDisplay, clear }
-})();
+};
 
 const calc = (() => {
     const add = (x, y) => x + y;
@@ -136,96 +135,106 @@ const calc = (() => {
     return { add, mult, sub, div, exp, operation}
 })();
 
+const clearButton = (() => {
+    let button = elementBuilder('button', 'button', elements.buttonDiv);
+    button.id = "clear";
+    button.textContent = "Clear";
+})();
+
 const calcLogic = (() => {
-    function buttonListener(buttonElement, displayContents) {
+    
+    function buttonListener(buttonElement, displayContents, displayObj) {
         buttonElement.addEventListener("click", function numEvent() {
-            display.addToArray(displayContents);
+            displayObj.addToArray(displayContents);
         });
     }
 
-    const addListener = (newSet) => {
+    const addListener = (newSet, displayObj) => {
         for (i = 0; i < newSet.elementArray.length; i++) {
             let newButton = newSet.elementArray[i];
             let num = newButton.textContent
-            buttonListener(newButton, num)
+            buttonListener(newButton, num, displayObj)
             };
     }
 
-    function equals(operator, numOne) {
+    function equals(operator, numOne, displayObj) {
         let newCalcSet = calcButtons()
         let numTwo = parseFloat(elements.calcDisplay.textContent);
         for (i = 0; i < newCalcSet.elementArray.length -1; i++) {
             let newOpButton = newCalcSet.elementArray[i];
             newOpButton.addEventListener("click", function newOp() {
-                display.clearArray();
+                displayObj.clearArray();
                 let result = calc.operation(operator, numOne, numTwo);
-                display.pushToDisplay(result);
+                displayObj.pushToDisplay(result);
                 operator = newOpButton.textContent;
                 let newNumSet = numButtons()
-                secondNum(newNumSet, result, operator);
+                secondNum(newNumSet, result, operator, displayObj);
             });
         }
         let equalsButton = document.getElementById("=");
         equalsButton.addEventListener("click", function calculation() {
             let result = calc.operation(operator, numOne, numTwo);
-            display.clearArray();
-            display.pushToDisplay(result);
+            displayObj.clearArray();
+            displayObj.pushToDisplay(result);
             let newCalcSet = calcButtons()
-            getOperator(newCalcSet);
+            getOperator(newCalcSet, displayObj);
         });
     }
 
-    const secondNum = (newSet, numOne, operator) => {
+    const secondNum = (newSet, numOne, operator, displayObj) => {
         for (i = 0; i < newSet.elementArray.length; i++) {
             let newButton = newSet.elementArray[i];
             let num = newButton.textContent;
             newButton.addEventListener("click", function operation() {
-                display.addToArray(num);
-                equals(operator, numOne);
+                displayObj.addToArray(num);
+                equals(operator, numOne, displayObj);
             });
         };
     }
 
-    function getOperator(calcSet) {
+    function getOperator(calcSet, displayObj) {
         for (i = 0; i < calcSet.operators.length-1; i++) {
             let operator = calcSet.operators[i];
             let newButton = calcSet.elementArray[i];
             newButton.addEventListener("click", function calcEvent() {
                 let numOne = parseFloat(elements.calcDisplay.textContent);
-                display.clearArray();
-                display.pushToDisplay(operator);
+                displayObj.clearArray();
+                displayObj.pushToDisplay(operator);
                 let newNumSet = numButtons()
-                secondNum(newNumSet, numOne, operator);
+                secondNum(newNumSet, numOne, operator, displayObj);
             }); 
         }
     }
 
     function operate() {
+        let newdisplay = display()
+        clearEvent(newdisplay);
+        console.log(newdisplay.array)
         let numSet = numButtons();
         let calcSet = calcButtons();
-        addListener(numSet);
-        getOperator(calcSet);
+        addListener(numSet, newdisplay);
+        getOperator(calcSet, newdisplay);
         let equalsButton = document.getElementById("=");
         equalsButton.addEventListener("click", function calculation() {
             let result = elements.calcDisplay.textContent
-            display.clearArray();
-            display.pushToDisplay(result);
+            newdisplay.clearArray();
+            newdisplay.pushToDisplay(result);
             let newCalcSet = calcButtons()
-            getOperator(newCalcSet);
+            getOperator(newCalcSet, newdisplay);
         });
     }
 
-    const clearButton = (() => {
-        let button = elementBuilder('button', 'button', elements.buttonDiv);
-        button.id = "clear";
-        button.textContent = "Clear";
-
+    const clearEvent = (displayObj) => {
+        let button = document.getElementById("clear")
+        console.log(button)
         function clearAll() {
-            display.clear()
-            operate()
+            displayObj.clearArray();
+            displayObj.clear()
+            operate();
         }
         button.addEventListener("click", clearAll);
-    })();
+    };
+
 
     operate()
 })();
